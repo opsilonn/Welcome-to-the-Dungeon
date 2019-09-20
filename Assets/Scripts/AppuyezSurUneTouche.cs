ecd7254fddd0bac15bee79c832c3f76c1a730544
Hugues Begeot <hugues.begeot@efrei.net>
2019-09-20 08:10:04 +0200

    Stuff...
    yeah, some stuff

---
+++ Assets/Scripts/AppuyezSurUneTouche.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+public class AppuyezSurUneTouche : MonoBehaviour
+{
+    public GameObject toClose;
+    public GameObject toOpen;
+
+
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            toOpen.SetActive(true);
+            toClose.SetActive(false);
+        }
+    }
+}
